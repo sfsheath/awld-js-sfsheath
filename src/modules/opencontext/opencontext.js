de6118e6683ae3e7f56a3fd5e10755d3fc45e5df
Sebastian Heath <sebastian.heath@gmail.com>
2012-05-06 10:25:45 -0400

    OpenContext Module, test page

---
+++ src/modules/opencontext/opencontext.js
@@ -0,0 +1,19 @@
+// Module: OpenContext HTML
+
+define(['jquery'], function($) {
+    return {
+        name: 'Open Context Resource',
+        dataType: 'xml',
+        type: 'object',
+        toDataUri: function(uri) {
+            return uri;
+        },
+        parseData: function(xml) {
+            var getText = awld.accessor(xml);
+            return {
+                name: "OpenContext " + getText('[id = "item_name"]'),
+                description: getText('[id = "item_class"]')
+            };
+        },
+    };
+});
