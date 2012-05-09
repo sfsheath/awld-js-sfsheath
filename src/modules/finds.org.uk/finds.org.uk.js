26a293d17235c12000d238b546e5762a3310098e
Sebastian Heath <sebastian.heath@gmail.com>
2012-05-09 01:26:06 -0400

    more modules

---
+++ src/modules/finds.org.uk/finds.org.uk.js
@@ -0,0 +1,17 @@
+// Module: OpenContext HTML
+
+define(['jquery'], function($) {
+    return {
+        name: 'Portable Antiquities Scheme Object',
+        dataType: 'html',
+        type: 'object',
+        parseData: function(html) {
+            var getText = awld.accessor(html);
+            var imageURI = 'http://finds.org.uk/' + getText('a[rel="lightbox"] img', 'src')
+            return {
+                name: "PAS " + getText('a[rel="lightbox"]', 'title'),
+                description: '<br/><img style="max-width:150" src="'+imageURI+'"/>'
+            };
+        },
+    };
+});
