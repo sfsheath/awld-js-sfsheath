f7580d4097f6f0112b94daf819ed841a22a69fd1
Sebastian Heath <sebastian.heath@gmail.com>
2012-05-16 15:07:10 -0400

    typo

--- registry.js
+++ registry.js
@@ -1,6 +1,6 @@
 define('registry', {
     'http://arachne.uni-koeln.de/item/': 'arachne.uni-koeln.de/arachne.uni-koeln.de',
-    'http://arachne.uni-koeln.de/enity/': 'arachne.uni-koeln.de/arachne.uni-koeln.de',
+    'http://arachne.uni-koeln.de/entity/': 'arachne.uni-koeln.de/arachne.uni-koeln.de',
     'http://data.perseus.org/citations/urn:cts': 'perseus/urn-cts',
     'http://data.perseus.org/people/smith': 'perseus/smith',
     'http://ecatalogue.art.yale.edu/detail.htm?objectId=': 'ecatalogue.art.yale.edu/ecatalogue.art.yale.edu',
