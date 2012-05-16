4aa14dd5c968d27c69fd7cecee33e55c3c45e4dd
Sebastian Heath <sebastian.heath@gmail.com>
2012-05-16 15:28:47 -0400

    arachne entity uris have broken redirect so ignore for now.

--- modules/arachne.uni-koeln.de/arachne.uni-koeln.de.js
+++ modules/arachne.uni-koeln.de/arachne.uni-koeln.de.js
@@ -1,4 +1,4 @@
-// Module: Arachne Item HTML
+// Module: Arachne Item/Entity HTML
 
 define(['jquery'], function($) {
     return {
@@ -7,9 +7,11 @@ define(['jquery'], function($) {
         type: 'object',
         parseData: function(html) {
             var getText = awld.accessor(html);
-
+            if (DEBUG) { console.log(html) };
             var imageURI = getText('img[src*="image.php"]', 'src');
+            if (DEBUG) { console.log(imageURI) };
             imageURI = typeof imageURI === 'string'? imageURI : imageURI[0];
+            if (DEBUG) { console.log(imageURI) };
             imageURI = 'http://arachne.uni-koeln.de/arachne/'+imageURI;
 
             return {
