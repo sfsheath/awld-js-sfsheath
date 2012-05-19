fcef5c036f7b508710ed39282dbea3a3da47794d
Sebastian Heath <sebastian.heath@gmail.com>
2012-05-19 18:44:40 -0400

    wiki images

--- modules/wikipedia/page.js
+++ modules/wikipedia/page.js
@@ -12,9 +12,19 @@ define(function() {
         parseData: function(data) {
             data = data && data.parse || {};
             var $content = $('<div>' + data.text['*'] + '</div>');
+
+            var description = $('p', $content).first().html();
+            description = description.replace(/href="\//g,'href="http://en.wikipedia.org/')
+
+            var imageURI = $('.image img',$content);
+            imageURI = typeof imageURI.first()[0] === 'object' ? imageURI = 'http:'+imageURI.first()[0].getAttribute('src') : ''; 
+
+            console.log ("########",imageURI);
+
             return {
                 name: data.title,
-                description: $('p', $content).first().html()
+                description: description,
+                imageURI: imageURI,
             };
         }
     };
